923975f1d036566f3dedcfc9c7505d167b8ed2e1
keychera <kyogatama@gmail.com>
2024-12-07 00:52:56 +0900

    aoc2024 day1 pt1

---
+++ src/day1/day1.clj
@@ -0,0 +1,20 @@
+(ns day1.day1
+  (:require
+   [babashka.fs :as fs]
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
+
+;; part 1
+(with-open [rdr (io/reader (fs/file (.getParent (fs/file *file*)) "input.txt"))]
+  (let [[list1 list2]
+        (->>  (line-seq rdr)
+              (mapv (fn [line] (->> (str/split line #"\s+") (map Integer/parseInt))))
+              (flatten)
+              (map-indexed vector)
+              (group-by (fn [el] (even? (first el))))
+              (vals)
+              (map (fn [li] (sort (mapv last li)))))
+        distances (->> (interleave list1 list2)
+                       (partition 2)
+                       (map (fn [[a b]] (abs (- a b)))))]
+    (reduce + distances)))
