d0df5550d538f02fc014f71bcc9975f0d3af7564
keychera <kyogatama@gmail.com>
2024-12-07 01:15:34 +0900

    day1 pt2

--- src/day1/day1.clj
+++ src/day1/day1.clj
@@ -14,7 +14,26 @@
               (group-by (fn [el] (even? (first el))))
               (vals)
               (map (fn [li] (sort (mapv last li)))))
-        distances (->> (interleave list1 list2)
-                       (partition 2)
-                       (map (fn [[a b]] (abs (- a b)))))]
+        
+        distances
+        (->> (interleave list1 list2)
+             (partition 2)
+             (map (fn [[a b]] (abs (- a b)))))]
     (reduce + distances)))
+
+
+;; part 2
+(with-open [rdr (io/reader (fs/file (.getParent (fs/file *file*)) "input.txt"))]
+  (let [[list1 list2]
+        (->>  (line-seq rdr)
+              (mapv (fn [line] (->> (str/split line #"\s+") (map Integer/parseInt))))
+              (flatten)
+              (map-indexed vector)
+              (group-by (fn [el] (even? (first el))))
+              (vals)
+              (map (fn [li] (sort (mapv last li)))))
+
+        similarity
+        (->> list1 (map (fn [el1]
+                          (->> list2 (filter (fn [el2] (= el1 el2))) count (* el1)))))]
+    (reduce + similarity)))
