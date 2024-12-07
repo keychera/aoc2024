9881276f986b8fec1907090cec310657891c5d34
keychera <kyogatama@gmail.com>
2024-12-07 20:14:26 +0900

    compare answer

--- src/day1/day1.clj
+++ src/day1/day1.clj
@@ -37,3 +37,11 @@
         (->> list1 (map (fn [el1]
                           (->> list2 (filter (fn [el2] (= el1 el2))) count (* el1)))))]
     (reduce + similarity)))
+
+
+(comment
+  ;; comparing with https://github.com/reflechant/aoc2024-clj/blob/main/src/reflechant/aoc2024/day01/part1.clj
+  ;; solution above can be better done with below
+  (apply map vector [[1 2] [3 4] [5 6]])
+  (apply map - [[1 2] [3 4] [5 6]])
+  )
