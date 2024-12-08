afa55771526c6d2604c64fc42b9648663a597285
keychera <kyogatama@gmail.com>
2024-12-08 16:09:29 +0900

    day3 pt1

---
+++ src/day3/day3.clj
@@ -0,0 +1,27 @@
+(ns day3.day3
+  (:require
+   [babashka.fs :as fs]
+   [clojure.string :as str])
+  (:import [java.util Scanner]))
+
+;; part 1
+(with-open [scanner (.. (Scanner. (fs/file (.getParent (fs/file *file*)) "input.txt"))
+                        (useDelimiter "\\)"))]
+  (loop [result 0]
+    (if (.hasNext scanner)
+      (let [delimited    (.next scanner)
+            [found? a b] (re-find (re-matcher #"mul\((\d{1,3}),(\d{1,3})$" delimited))
+            mul-result   (if found? (* (Integer/valueOf a) (Integer/valueOf b)) 0)]
+        (println delimited found? a b)
+        (recur (+ result mul-result)))
+      result)))
+
+
+(comment
+  (->> (str/split "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))" #"\)")
+       (mapv (fn [s]
+               (let [[found? a b] (re-find (re-matcher #"mul\((\d{1,3}),(\d{1,3})$" s))]
+                 (println found?)
+                 (if found? (* (Integer/valueOf a) (Integer/valueOf b)) 0))))
+       (reduce +)))
+
