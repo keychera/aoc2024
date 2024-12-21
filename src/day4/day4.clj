3b9fb4dcf11fa6a07ca36ca64a9dd48c8265efd2
keychera <kyogatama@gmail.com>
2024-12-22 00:49:07 +0900

    day4 pt2

--- src/day4/day4.clj
+++ src/day4/day4.clj
@@ -4,7 +4,7 @@
    [clojure.string :as str]
    [clojure.java.io :as io]))
 
-
+;; part 1
 (with-open [reader (io/reader (fs/file (.getParent (fs/file *file*)) "input.txt"))]
   (->> (loop [scan {} row 0
               [line & line-rem] (line-seq reader)]
@@ -43,5 +43,37 @@
        vals
        (map rest) flatten (filter #{"XMAS" "SAMX"}) count))
 
+
+;; part 2
+(with-open [reader (io/reader (fs/file (.getParent (fs/file *file*)) "input.txt"))]
+  (->> (loop [scan {} row 0
+              [line & line-rem] (line-seq reader)]
+         (let [chars
+               (loop [scan scan col 0
+                      [ch & ch-rem] line]
+                 (if (some? ch)
+                   (let [[ul<-ch] (scan [(dec row) (dec col)])
+                         [ch->ur [half-XMAS? ul<-A A->ur]] (scan [(dec row) (inc col)])
+                         [l<-ch<-ch _ [almost-XMAS? ul<-A' A->ur']] (scan [row (- col 2)])]
+                     (recur (assoc scan [row col]
+                                   [ch
+                                    (when (and (= ch \A) (every? #{\S \M} [ul<-ch ch->ur]))
+                                      [:half-xmas ul<-ch ch->ur])
+                                    (when (and half-XMAS? (#{\S \M} ch) (not= A->ur ch))
+                                      [:almost-xmas ul<-A A->ur])
+                                    (when almost-XMAS?
+                                      (let [x-mas [ul<-A' A->ur' l<-ch<-ch ch]]
+                                        (when (= 2
+                                                 (count (filter #{\M} x-mas))
+                                                 (count (filter #{\S} x-mas)))
+                                          x-mas)))])
+                            (inc col) ch-rem))
+                   scan))
+               scan  (into scan chars)]
+           (if (some? line)
+             (recur scan (inc row) line-rem)
+             (->> scan
+                  (filter (fn [[_ v]] (some? (get v 3))))
+                  count))))))
 
 
